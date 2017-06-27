1494d1b1ff369c073a88c90fbd668c59025e8ac8
mts <michaelsmith101@users.noreply.github.com>
2017-06-26 21:45:32 -0700

    Finished Checkpoint 7 - Typo

--- resources/scripts/landing.js
+++ resources/scripts/landing.js
@@ -3,7 +3,7 @@ var animatePoints = function() {
 
   var points = document.getElementsByClassName('point');
 
-  for (i=0;i<points.length;i++){
+  for (var i=0;i<points.length;i++){
     points[i].style.opacity = 1;
     points[i].style.transform = "scaleX(1) translateY(0)"
     points[i].style.mstransform = "scaleX(1) translateY(0)"
