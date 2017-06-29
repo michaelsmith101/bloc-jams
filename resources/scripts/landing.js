d016e159fcdef6eb5ae4c67e6018a423c0a365c3
mts <michaelsmith101@users.noreply.github.com>
2017-06-28 20:45:21 -0700

    Finished Event Listeners

--- resources/scripts/landing.js
+++ resources/scripts/landing.js
@@ -1,28 +1,35 @@
-//Global Variables
-var pointsArray = document.getElementsByClassName('point');
+//Global Scope
+var points = document.getElementsByClassName('point');
 //Selling Points Animation
-var animatePoints = function(points) {
-  for (var i=0;i<points.length;i++){
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)"
-    points[i].style.mstransform = "scaleX(1) translateY(0)"
-    points[i].style.Webkittransform = "scaleX(1) translateY(0)"
+var animatePoints = function() {
+
+  var revealPoint = function(index){
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)"
+    points[index].style.mstransform = "scaleX(1) translateY(0)"
+    points[index].style.Webkittransform = "scaleX(1) translateY(0)"
+  };
+
+  for (i=0;i<points.length;i++){
+    revealPoint(i);
   }
 };
-//Adding the scroll event
-window.onload = function() {
-  //automatically perform the animation if the users screen is taller than 950px
-  if (window.innerHeight > 950){
-    animatePoints(pointsArray);
-  }
-  //if not perform an animation on scroll when they reach 200px below the fold
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+//Event Listeners
+  window.onload = function() {
 
-  window.addEventListener('scroll', function(event){
-    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-      animatePoints(pointsArray);
-    }
-  });
+    //Animate automatically if the scren is tall
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
 
-}
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+     });
+ }
+
+//
