dafbecb0fc92ce0b96d38f3bbf757748e44825c6
mts <michaelsmith101@users.noreply.github.com>
2017-06-30 22:18:03 -0700

    Finished Checkpoint. Added Click Event.

--- resources/scripts/album.js
+++ resources/scripts/album.js
@@ -20,13 +20,28 @@ var albumMarconi = {
   artist: 'Guglielmo Marconi',
   label: 'EM',
   year: '1909',
-  albumArtUrl: 'assets/images/album_covers/20.png',
+  albumArtUrl: './resources/images/album_covers/20.png',
   songs: [
     { title: 'Hello, Operator?', duration: '1:01' },
     { title: 'Ring, ring, ring', duration: '5:01' },
     { title: 'Fits in your pocket', duration: '3:21'},
     { title: 'Can you hear me now?', duration: '3:14' },
     { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+//Example Album
+var albumBabe = {
+  title: 'Childish Bambino',
+  artist: 'Babe Ruth',
+  label: 'NYY',
+  year: '1914',
+  albumArtUrl: './resources/images/album_covers/19.png',
+  songs: [
+    { title: 'Take', duration: '1:00' },
+    { title: 'Me', duration: '2:00' },
+    { title: 'Out To The', duration: '3:00'},
+    { title: 'Ball', duration: '4:00' },
+    { title: 'Game', duration: '5:00'}
   ]
 };
 //create song row
@@ -68,3 +83,21 @@ var createSongRow = function(songNumber, songName, songLength) {
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
 };
+
+var cycleAlbums = [albumPicasso, albumMarconi, albumBabe];
+var curAlbum = 0;
+
+function nextAlbum(){
+  if(curAlbum === cycleAlbums.length-1){
+    curAlbum = 0;
+  } else {
+    curAlbum++
+  }
+  return cycleAlbums[curAlbum]
+};
+
+function moveToNextAlbum(){
+  setCurrentAlbum(nextAlbum());
+}
+
+document.getElementsByTagName("img")[1].addEventListener("click", moveToNextAlbum);
