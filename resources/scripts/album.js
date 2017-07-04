40b16c3b535cacd73366d51dabbe0abc3874eaca
mts <michaelsmith101@users.noreply.github.com>
2017-07-03 21:38:31 -0700

    Finished Checkpoint 13

--- resources/scripts/album.js
+++ resources/scripts/album.js
@@ -140,7 +140,11 @@ window.onload = function() {
     //song play button appear
     songListContainer.addEventListener('mouseover', function(event) {
       if (event.target.parentElement.className === 'album-view-song-item') {
-        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
       }
     });
 
