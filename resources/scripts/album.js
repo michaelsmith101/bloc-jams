2205089dcaa1a65b1905bb3651eeb5f0aa0ef879
mts <michaelsmith101@users.noreply.github.com>
2017-07-04 22:56:43 -0700

    FIxed event listeners for Checkpoint 12 and 13

--- resources/scripts/album.js
+++ resources/scripts/album.js
@@ -136,39 +136,14 @@ var clickHandler = function(targetElement) {
 };
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
-
-    //song play button appear
-    songListContainer.addEventListener('mouseover', function(event) {
-      if (event.target.parentElement.className === 'album-view-song-item') {
-        var songItem = getSongItem(event.target);
-
-            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-                songItem.innerHTML = playButtonTemplate;
-            }
-      }
-    });
-
-    for (var i = 0; i < songRows.length; i++) {
-         songRows[i].addEventListener('mouseleave', function(event) {
-           // #1
-             var songItem = getSongItem(event.target);
-             var songItemNumber = songItem.getAttribute('data-song-number');
-
-             // #2
-             if (songItemNumber !== currentlyPlayingSong) {
-                 songItem.innerHTML = songItemNumber;
-             }
-         });
-         songRows[i].addEventListener('click', function(event) {
-                clickHandler(event.target);
-        });
-     };
+    playButtonInteraction();
 
     //album click functionality
     var albums = [albumPicasso, albumMarconi, albumBabe];
     var index = 1;
     albumImage.addEventListener('click',function(event){
       setCurrentAlbum(albums[index]);
+      playButtonInteraction();
       index++;
       if(index == albums.length){
         index = 0;
@@ -176,3 +151,32 @@ window.onload = function() {
     });
 
 };
+function playButtonInteraction(){
+
+      //song play button appear
+      songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+          var songItem = getSongItem(event.target);
+
+              if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                  songItem.innerHTML = playButtonTemplate;
+              }
+        }
+      });
+
+      for (var i = 0; i < songRows.length; i++) {
+           songRows[i].addEventListener('mouseleave', function(event) {
+             // #1
+               var songItem = getSongItem(event.target);
+               var songItemNumber = songItem.getAttribute('data-song-number');
+
+               // #2
+               if (songItemNumber !== currentlyPlayingSong) {
+                   songItem.innerHTML = songItemNumber;
+               }
+           });
+           songRows[i].addEventListener('click', function(event) {
+                  clickHandler(event.target);
+          });
+       };
+}
