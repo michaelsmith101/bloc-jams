855c3c7210eef08e6b435f42de7928f0adaf70ee
mts <michaelsmith101@users.noreply.github.com>
2017-07-11 20:00:21 -0700

    Added Buzz Library and MP3s

--- resources/scripts/fixtures.js
+++ resources/scripts/fixtures.js
@@ -6,11 +6,11 @@ var albumPicasso = {
   year: '1881',
   albumArtUrl: './resources/images/album_covers/01.png',
   songs: [
-    {title: 'Blue', duration: '4:26'},
-    {title: 'Green', duration: '3:14'},
-    {title: 'Red' ,duration: '5:01'},
-    {title: 'Pink' ,duration: '3:21'},
-    {title: 'Magenta', duration: '2:15'}
+    { title: 'Blue', duration: '4:26', audioUrl: '../music/blue' },
+   { title: 'Green', duration: '3:14', audioUrl: '../music/green' },
+   { title: 'Red', duration: '5:01', audioUrl: '../music/red' },
+   { title: 'Pink', duration: '3:21', audioUrl: '../music/pink' },
+   { title: 'Magenta', duration: '2:15', audioUrl: '../music/magenta' }
   ]
 
 };
