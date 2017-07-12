ed11287faa0e438a6d513d3b3650ebc45e0e5514
mts <michaelsmith101@users.noreply.github.com>
2017-07-11 21:19:18 -0700

    Converted Durations to Seconds

--- resources/scripts/fixtures.js
+++ resources/scripts/fixtures.js
@@ -6,11 +6,12 @@ var albumPicasso = {
   year: '1881',
   albumArtUrl: './resources/images/album_covers/01.png',
   songs: [
-    { title: 'Blue', duration: '4:26', audioUrl: './resources/music/blue' },
-   { title: 'Green', duration: '3:14', audioUrl: './resources/music/green' },
-   { title: 'Red', duration: '5:01', audioUrl: './resources/music/red' },
-   { title: 'Pink', duration: '3:21', audioUrl: './resources/music/pink' },
-   { title: 'Magenta', duration: '2:15', audioUrl: './resources/music/magenta' }
+    { title: 'Blue', duration: 161.71, audioUrl: './resources/music/blue' },
+   { title: 'Green', duration: 103.96, audioUrl: './resources/music/green' },
+   { title: 'Red', duration: 268.45, audioUrl: './resources/music/red' },
+   { title: 'Pink', duration: 153.14, audioUrl: './resources/music/pink' },
+   { title: 'Magenta', duration: 374.22, audioUrl: './resources/music/magenta' }
+
   ]
 
 };
