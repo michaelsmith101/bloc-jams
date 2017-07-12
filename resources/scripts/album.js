411d063c3db7e568583129f3272632a0a1e76646
mts <michaelsmith101@users.noreply.github.com>
2017-07-11 17:38:30 -0700

    Move Album Data

--- resources/scripts/album.js
+++ resources/scripts/album.js
@@ -15,52 +15,6 @@ $(window).load(function() {
     });
 });
 
-//Picasso Album
-var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: './resources/images/album_covers/01.png',
-  songs: [
-    {title: 'Blue', duration: '4:26'},
-    {title: 'Green', duration: '3:14'},
-    {title: 'Red' ,duration: '5:01'},
-    {title: 'Pink' ,duration: '3:21'},
-    {title: 'Magenta', duration: '2:15'}
-  ]
-
-};
-//Marconi Album
-var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
-  label: 'EM',
-  year: '1909',
-  albumArtUrl: './resources/images/album_covers/20.png',
-  songs: [
-    { title: 'Hello, Operator?', duration: '1:01' },
-    { title: 'Ring, ring, ring', duration: '5:01' },
-    { title: 'Fits in your pocket', duration: '3:21'},
-    { title: 'Can you hear me now?', duration: '3:14' },
-    { title: 'Wrong phone number', duration: '2:15'}
-  ]
-};
-//Example Album
-var albumBabe = {
-  title: 'Childish Bambino',
-  artist: 'Babe Ruth',
-  label: 'NYY',
-  year: '1914',
-  albumArtUrl: './resources/images/album_covers/19.png',
-  songs: [
-    { title: 'Take', duration: '1:00' },
-    { title: 'Me', duration: '2:00' },
-    { title: 'Out To The', duration: '3:00'},
-    { title: 'Ball', duration: '4:00' },
-    { title: 'Game', duration: '5:00'}
-  ]
-};
 //create song row
 var currentlyPlayingSong = null;
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
